0068f0a9ab00268d41b173060dbf836fe5f2e167
Massa Adonaï <massaadonai@gmail.com>
2024-10-12 20:32:05 +0200

    Initiliser Pygame

--- python/morpion.py
+++ python/morpion.py
@@ -12,3 +12,16 @@ BLANC = (255, 255, 255)
 NOIR = (0, 0, 0)
 ROUGE = (213, 50, 80)
 
+# Initialiser Pygame
+pygame.init()
+fenetre = pygame.display.set_mode((LARGUR, HAUTEUR))
+pygame.display.set_caption("Morpion")
+
+# Créer le plateau
+plateau = [
+    [" " 
+     for _ in range(3)
+    ] 
+    for _ in range(3)
+]
+
