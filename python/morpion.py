d5c9ef73e6ab3758f780c6654681d11a40c75264
Massa Adonaï <massaadonai@gmail.com>
2024-10-12 20:06:00 +0200

    ajout variable

--- python/morpion.py
+++ python/morpion.py
@@ -1,1 +1,14 @@
-# Author: Massa Adonaï
+# bibliothèque utilisée
+import pygame
+import sys
+
+# Dimensions de la fenêtre
+LARGUR = 300
+HAUTEUR = 300
+LARGEUR_CASE = 100
+
+# Couleurs
+BLANC = (255, 255, 255)
+NOIR = (0, 0, 0)
+ROUGE = (213, 50, 80)
+
