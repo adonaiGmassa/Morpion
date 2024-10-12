907d02bfb323889919a18cb81a7f3a42880edc5c
Massa Adonaï <massaadonai@gmail.com>
2024-10-12 20:32:46 +0200

    Fonction pour dessiner le plateau

--- python/morpion.py
+++ python/morpion.py
@@ -25,3 +25,42 @@ plateau = [
     for _ in range(3)
 ]
 
+# Fonction pour dessiner le plateau
+def DESSINER_PLATEAU():
+
+    fenetre.fill(BLANC)  # Remplit la fenêtre avec la couleur blanche en arrière-plan
+
+    for i in range(1, 3):# Dessine les lignes horizontales du plateau
+        # Dessine une ligne horizontale
+        pygame.draw.line(fenetre, NOIR, (0, i * LARGEUR_CASE), (LARGUR, i * LARGEUR_CASE), 2)
+        # Dessine une ligne verticale
+        pygame.draw.line(fenetre, NOIR, (i * LARGEUR_CASE, 0), (i * LARGEUR_CASE, HAUTEUR), 2)
+        
+        # Parcourt chaque ligne du plateau
+        for i in range(3):
+    
+            # Parcourt chaque colonne du plateau
+            for j in range(3):
+
+                # Vérifie si la case contient un "X"
+                if plateau[i][j] == "X":
+
+                    # Dessine la première diagonale du "X"
+                    pygame.draw.line(
+                        fenetre, ROUGE,
+                        (j * LARGEUR_CASE + 20, i * LARGEUR_CASE + 20),
+                        (j * LARGEUR_CASE + 80, i * LARGEUR_CASE + 80), 2
+                        )  
+
+                    # Dessine la deuxième diagonale du "X"
+                    pygame.draw.line(
+                        fenetre, ROUGE,
+                        (j * LARGEUR_CASE + 80, i * LARGEUR_CASE + 20),
+                        (j * LARGEUR_CASE + 20, i * LARGEUR_CASE + 80),2
+                        ) 
+                
+                # Vérifie si la case contient un "O"
+                elif plateau[i][j] == "O":
+                    # Dessine un cercle pour le "O"
+                    pygame.draw.circle(fenetre, NOIR, (j * LARGEUR_CASE + 50, i * LARGEUR_CASE + 50),40,2)  
+
